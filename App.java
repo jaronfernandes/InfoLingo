4d8c29226f236934fa7f4b9a7f97a710042ab6ea
Jaiz183 <jaizepic@gmail.com>
2023-09-28 20:54:04 -0400

    Basically done, need finishing touches.

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        SampleAPICall sampleAPICall = new SampleAPICall();
+        System.out.println(sampleAPICall.getSampleNews("roblox"));
+    }
+}
--- SampleAPICall.java
+++ SampleAPICall.java
@@ -4,6 +4,11 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import org.json.*;
+
+import javax.swing.*;
 
 public class SampleAPICall {
     private static final String BASE_URL = "https://newsapi.org/v2/";
@@ -12,10 +17,10 @@ public class SampleAPICall {
     private static final String API_TOKEN = System.getenv("API_TOKEN");
 
     public static void main(String[] args) {
-        System.out.println(getSampleNews("roblox"));
+
     }
 
-    private static Article[] getSampleNews(String searchTerm) {
+    public Article[] getSampleNews(String searchTerm) {
         try {
             URI uri = new URI(BASE_URL + "everything/?q=bitcoin&apiKey=" + API_TOKEN);
             HttpClient client = HttpClient.newHttpClient();
@@ -24,11 +29,24 @@ public class SampleAPICall {
                     GET().
                     build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
+            String body = response.body();
+            JSONObject data = new JSONObject(body);
+
+            // Retrieve first article.
+            JSONArray articles = data.getJSONArray("articles");
+            JSONObject firstArticle = articles.getJSONObject(0);
+
+            // Retrieve details of first article.
+            String author = firstArticle.getString("author");
+            String description = firstArticle.getString("description");
+            Article article = new Article(description, author);
+            System.out.println(description);
+            System.out.println(article.getContent());
         } catch (Exception e) {
-            System.out.println(e.getStackTrace());
+            System.out.println(Arrays.toString(e.getStackTrace()));
         }
 
+        // Return all articles here.
         return new Article[0];
     }
 
@@ -37,8 +55,16 @@ public class SampleAPICall {
         private String author;
         private String title;
         private String url;
+        private String source;
 
-        public Article() {
+        public Article(String content, String author) {
+            this.author = author;
+            this.content = content;
+
+        }
+
+        public String getContent() {
+            return content;
         }
     }
 }
