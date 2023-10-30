6d3f3250d67c825da52d47dcfa47efe1e38f03d5
seth0 <seth0712@gmail.com>
2023-10-29 22:52:30 -0400

    Added controller and input data for article retrieval

--- src/interface_adapter/ArticleRetrievalController.java
+++ src/interface_adapter/ArticleRetrievalController.java
@@ -1,4 +1,15 @@
 package interface_adapter;
 
 public class ArticleRetrievalController {
+    final ArticleRetrievalInputBoundary articleRetrievalInteractor;
+
+
+    public ArticleRetrievalController(ArticleRetrievalInputBoundary articleRetrievalInteractor) {
+        this.articleRetrievalInteractor = articleRetrievalInteractor;
+    }
+
+    public void execute(String query){
+        ArticleRetrievalInputData articleRetrievalInputData = new ArticleRetrievalInputData(query);
+        articleRetrievalInteractor.execute(articleRetrievalInputData);
+    }
 }
