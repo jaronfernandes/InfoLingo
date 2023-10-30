bf4759f631752d21d574cdd3b240aec6cea26258
fern1394 <fernandesjaron@gmail.com>
2023-10-29 23:31:56 -0400

    Worked on hooking up interactor with DAO

--- src/use_case/ArticleRetrievalDataAccessInterface.java
+++ src/use_case/ArticleRetrievalDataAccessInterface.java
@@ -2,8 +2,10 @@ package use_case;
 
 import entity.Article;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface ArticleRetrievalDataAccessInterface {
     List<Article> getArticles();
+    Article[] getSampleNews();
 }
--- src/use_case/ArticleRetrievalInteractor.java
+++ src/use_case/ArticleRetrievalInteractor.java
@@ -1,8 +1,33 @@
 package use_case;
 
+import data_access.APIDataAccessObject;
+import entity.Article;
+
+import java.util.ArrayList;
+
 public class ArticleRetrievalInteractor implements ArticleRetrievalInputBoundary {
+    private ArticleRetrievalInputData inputData;
+    private ArticleRetrievalOutputBoundary presenter;
+    private APIDataAccessObject articleRetrievalDataAccessObject;
+
+    public ArticleRetrievalInteractor(ArticleRetrievalInputData inputData, ArticleRetrievalOutputBoundary outputBoundary,
+                                      APIDataAccessObject articleRetrievalDataAccessObject) {
+        this.articleRetrievalDataAccessObject = articleRetrievalDataAccessObject;
+        this.inputData = inputData;
+        this.presenter = outputBoundary;
+    }
+
     @Override
     public void execute(ArticleRetrievalInputData inputData) {
+        try {
+            Article[] articles = articleRetrievalDataAccessObject.getSampleNews();
 
+            ArticleRetrievalOutputData outputData = new ArticleRetrievalOutputData(articles);
+            presenter.prepareSuccessView(outputData);
+        }
+        catch (Exception e) {
+            presenter.prepareFailView(e.toString());
+            System.out.println("what a failure.");
+        }
     }
 }
