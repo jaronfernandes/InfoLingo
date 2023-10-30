6d42dba4225c659573c5448540a2d26ac4032121
fern1394 <fernandesjaron@gmail.com>
2023-10-29 22:01:32 -0400

    added source entity

--- src/entity/Source.java
+++ src/entity/Source.java
@@ -1,4 +1,19 @@
 package entity;
 
 public class Source {
+    private String company;
+    private String language;
+
+    public Source(String company, String language) {
+        this.company = company;
+        this.language = language;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
 }
