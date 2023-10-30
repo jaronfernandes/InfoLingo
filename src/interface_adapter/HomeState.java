dc0cbe238b1505f0941fe5d1041ff04bcd4f09f8
Jaiz183 <jaizepic@gmail.com>
2023-10-29 21:10:46 -0400

    Pretty much did all the ViewModel, Presenter, OutputData, State stuff. Remains to be seen if everything works.

--- src/interface_adapter/HomeState.java
+++ src/interface_adapter/HomeState.java
@@ -1,4 +1,28 @@
 package interface_adapter;
+import entity.Article;
 
 public class HomeState {
+    Article[] articles;
+    String articleRetrievalError;
+
+    public HomeState(Article[] articles) {
+        this.articles = articles;
+        this.articleRetrievalError = "";
+    }
+
+    public Article[] getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Article[] articles) {
+        this.articles = articles;
+    }
+
+    public String getArticleRetrievalError() {
+        return articleRetrievalError;
+    }
+
+    public void setArticleRetrievalError(String articleRetrievalError) {
+        this.articleRetrievalError = articleRetrievalError;
+    }
 }
--- src/interface_adapter/HomeViewModel.java
+++ src/interface_adapter/HomeViewModel.java
@@ -1,4 +1,32 @@
 package interface_adapter;
 
-public class HomeViewModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class HomeViewModel extends ViewModel {
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private HomeState homeState;
+
+    public HomeViewModel(HomeState homeState) {
+        super("home");
+        this.homeState = homeState;
+    }
+
+    public HomeState getHomeState() {
+        return homeState;
+    }
+
+    public void setHomeState(HomeState homeState) {
+        this.homeState = homeState;
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.homeState);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
