fa39ae2a66c2babd60c3a67fe655fc76b8e88fde
Jaiz183 <jaizepic@gmail.com>
2023-11-08 21:56:02 -0500

    Completed input and output data (output data was created before with the rest of the group).

--- src/use_case/summarization/SummarizationInputData.java
+++ src/use_case/summarization/SummarizationInputData.java
@@ -2,7 +2,11 @@ package use_case.summarization;
 
 public class SummarizationInputData {
 
+<<<<<<< Updated upstream
     private String content;
+=======
+    private final String content;
+>>>>>>> Stashed changes
 
     public SummarizationInputData(String query) {
         this.content = query;
--- src/use_case/summarization/SummarizationInteractor.java
+++ src/use_case/summarization/SummarizationInteractor.java
@@ -7,25 +7,17 @@ import java.util.List;
 public class SummarizationInteractor implements SummarizationInputBoundary {
     private SummarizationOutputBoundary presenter;
     private SummarizationDataAccessInterface summarizationDataAccessObject;
+    private SummarizationOutputBoundary summarizationPresenter;
+    private String FAIL_TEXT = "";
 
     public SummarizationInteractor(SummarizationOutputBoundary outputBoundary,
                                    SummarizationDataAccessInterface articleRetrievalDataAccessObject) {
         this.summarizationDataAccessObject = articleRetrievalDataAccessObject;
-        this.presenter = outputBoundary;
+        this.summarizationPresenter = outputBoundary;
     }
 
     @Override
     public void execute(SummarizationInputData inputData) {
-        String summarizedText = summarizationDataAccessObject.summarizeArticle(inputData.getContent());
 
-        // TODO: figure out what the fail view would be
-        // TODO: remove this 1 == 0 nonsense (i dont want to activate fail view yet)
-        if (1 == 0) {
-            presenter.prepareFailView("Failed to retrieve any articles!");
-        }
-        else {
-            SummarizationOutputData outputData = new SummarizationOutputData(summarizedText);
-            presenter.prepareSuccessView(outputData);
-        }
     }
 }
