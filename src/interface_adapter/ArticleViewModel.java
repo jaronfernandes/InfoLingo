18acb4c01026f8a6bc77c214993a7a4ed1ac34eb
Jaiz183 <jaizepic@gmail.com>
2023-11-13 18:27:50 -0500

    Wrote ArticleViewModel, particularly code for property change supports and firing property change signals.

--- src/interface_adapter/ArticleViewModel.java
+++ src/interface_adapter/ArticleViewModel.java
@@ -1,4 +1,31 @@
 package interface_adapter;
 
-public class ArticleViewModel {
+import javax.swing.text.View;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class ArticleViewModel extends ViewModel {
+    private ArticleState articleState = new ArticleState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    public ArticleViewModel() {
+        super("article");
+    }
+
+    public ArticleState getArticleState() {
+        return articleState;
+    }
+
+    public void setArticleState(ArticleState articleState) {
+        this.articleState = articleState;
+    }
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.articleState);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
