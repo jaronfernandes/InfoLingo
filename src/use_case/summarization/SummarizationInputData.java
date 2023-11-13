e5e899b7906519f203323713d54b00226751b84c
Jaiz183 <jaizepic@gmail.com>
2023-11-13 18:17:31 -0500

    Completed creating InputData with appropriate fields.

--- src/use_case/summarization/SummarizationInputData.java
+++ src/use_case/summarization/SummarizationInputData.java
@@ -2,12 +2,7 @@ package use_case.summarization;
 
 public class SummarizationInputData {
 
-<<<<<<< Updated upstream
-    private String content;
-=======
     private final String content;
->>>>>>> Stashed changes
-
     public SummarizationInputData(String query) {
         this.content = query;
     }
