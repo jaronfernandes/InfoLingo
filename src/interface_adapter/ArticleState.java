5916143eb52b95c1dee6d17c2c5836a511f7b55f
Jaiz183 <jaizepic@gmail.com>
2023-11-13 18:21:03 -0500

    Completed creating ArticleState with appropriate fields.

--- src/interface_adapter/ArticleState.java
+++ src/interface_adapter/ArticleState.java
@@ -1,5 +1,22 @@
 package interface_adapter;
 
 public class ArticleState {
+    private String summarisedContent = "";
+    private String summarisationError = "";
 
+    public void setSummarisedContent(String summarisedContent) {
+        this.summarisedContent = summarisedContent;
+    }
+
+    public String getSummarisedContent() {
+        return summarisedContent;
+    }
+
+    public void setSummarisationError(String summarisationError) {
+        this.summarisationError = summarisationError;
+    }
+
+    public String getSummarisationError() {
+        return summarisationError;
+    }
 }
