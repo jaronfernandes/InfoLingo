d8e1a4af2261e0620b2683bdbcff1e24c70fc618
Jaiz183 <jaizepic@gmail.com>
2023-11-16 14:55:34 -0500

    Revert "Made minor mod to Article State, but doesn't affect anything outside my use case."
    This reverts commit 1b02c1aa14532d26946107575d454c514a2fe173.

--- src/interface_adapter/ArticleState.java
+++ src/interface_adapter/ArticleState.java
@@ -2,10 +2,7 @@ package interface_adapter;
 
 public class ArticleState {
     private String summarisedContent = "";
-    private String summarisationError;
-
-    private String headline = "";
-    private String originalContent = "";
+    private String summarisationError = "";
 
     public void setSummarisedContent(String summarisedContent) {
         this.summarisedContent = summarisedContent;
@@ -22,21 +19,4 @@ public class ArticleState {
     public String getSummarisationError() {
         return summarisationError;
     }
-
-    public void setHeadline(String headline) {
-        this.headline = headline;
-    }
-
-    public String getHeadline() {
-        return headline;
-    }
-
-    public void setOriginalContent(String originalContent) {
-        this.originalContent = originalContent;
-    }
-
-    public String getOriginalContent() {
-        return originalContent;
-    }
 }
-
