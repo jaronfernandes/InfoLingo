44d46caccd61fc3e955e37673ffa39fa7a150f89
Jaiz183 <jaizepic@gmail.com>
2023-11-25 23:26:25 -0500

    Modified View Manager to recognize property change events from Home View.

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -11,20 +11,35 @@ public class ViewManager implements PropertyChangeListener {
     private final CardLayout cardLayout;
     private final JPanel views;
     private ViewManagerModel viewManagerModel;
+    private HomeView homeView;
 
 
-    public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel) {
+    public ViewManager(JPanel views, CardLayout cardLayout, ViewManagerModel viewManagerModel, HomeView homeView) {
         this.views = views;
         this.cardLayout = cardLayout;
         this.viewManagerModel = viewManagerModel;
+        this.homeView = homeView;
+        this.homeView.addPropertyChangeListener(this);
         this.viewManagerModel.addPropertyChangeListener(this);
+
     }
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
+        System.out.println("Some sort of event.");
         if (evt.getPropertyName().equals("view")) {
+
             String viewModelName = (String) evt.getNewValue();
             cardLayout.show(views, viewModelName);
+        } else if (evt.getPropertyName().equals("switchView")) {
+            System.out.println("Recieved event to switch view.");
+            if (evt.getNewValue().equals("ArticleView")) {
+                cardLayout.show(views, "Article");
+            }
         }
+    }
+
+    public ViewManagerModel getViewManagerModel() {
+        return viewManagerModel;
     }
 }
