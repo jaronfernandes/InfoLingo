1d39de4af2e3e63876579719aa8e370648d58eb7
Jaron Fernandes <74974138+jaronfernandes@users.noreply.github.com>
2023-11-26 00:23:21 -0500

    Revert "Revert "Added Grouping to HomeView""

--- src/app/ArticleRetrievalUseCaseFactory.java
+++ src/app/ArticleRetrievalUseCaseFactory.java
@@ -1,21 +1,30 @@
 package app;
 
+import interface_adapter.GroupingViewModel;
 import interface_adapter.article_retrieval.ArticleRetrievalController;
 import interface_adapter.article_retrieval.ArticleRetrievalPresenter;
 import interface_adapter.HomeViewModel;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.grouping.GroupingController;
+import interface_adapter.grouping.GroupingPresenter;
 import use_case.article_retrieval.ArticleRetrievalDataAccessInterface;
 import use_case.article_retrieval.ArticleRetrievalInputBoundary;
 import use_case.article_retrieval.ArticleRetrievalInteractor;
 import use_case.article_retrieval.ArticleRetrievalOutputBoundary;
+import use_case.grouping.GroupingInputBoundary;
+import use_case.grouping.GroupingInteractor;
+import use_case.grouping.GroupingOutputBoundary;
 import view.HomeView;
 import view.ViewManager;
 
 public class ArticleRetrievalUseCaseFactory {
 
-    public static HomeView create(ViewManagerModel viewManagerModel, HomeViewModel homeViewModel, ArticleRetrievalDataAccessInterface articleRetrievalDataAccessObject) {
+    public static HomeView create(ViewManagerModel viewManagerModel, HomeViewModel homeViewModel,
+                                  ArticleRetrievalDataAccessInterface articleRetrievalDataAccessObject,
+                                  GroupingViewModel groupingViewModel) {
         ArticleRetrievalController articleRetrievalController = createArticleRetrievalUseCase(viewManagerModel, homeViewModel, articleRetrievalDataAccessObject);
-        return new HomeView(articleRetrievalController, homeViewModel);
+        GroupingController groupingController = createGroupingUseCase(viewManagerModel, groupingViewModel);
+        return new HomeView(articleRetrievalController, homeViewModel, groupingViewModel, groupingController);
     }
 
     private static ArticleRetrievalController createArticleRetrievalUseCase(ViewManagerModel viewManagerModel, HomeViewModel homeViewModel, ArticleRetrievalDataAccessInterface articleRetrievalDataAccessObject) {
@@ -23,5 +32,12 @@ public class ArticleRetrievalUseCaseFactory {
         ArticleRetrievalInputBoundary articleRetrievalInputBoundary = new ArticleRetrievalInteractor(articleRetrievalOutputBoundary, articleRetrievalDataAccessObject);
 
         return new ArticleRetrievalController(articleRetrievalInputBoundary);
+    }
+
+    private static GroupingController createGroupingUseCase(ViewManagerModel viewManagerModel, GroupingViewModel groupingViewModel) {
+        GroupingOutputBoundary groupingOutputBoundary = new GroupingPresenter(groupingViewModel);
+        GroupingInputBoundary groupingInputBoundary = new GroupingInteractor(groupingOutputBoundary);
+
+        return new GroupingController(groupingInputBoundary);
     }
 }
