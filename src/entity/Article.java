13c6a5954bab487ca4ee0f7d2ad3d6d86c0e9254
fern1394 <fernandesjaron@gmail.com>
2023-11-25 22:23:46 -0500

    added country and publishedAt instance attributes to TranslatedArticle and Article

--- src/entity/Article.java
+++ src/entity/Article.java
@@ -11,16 +11,30 @@ public class Article implements Media, ArticleInterface {
     private String author;
 
     private String URL;
+    private String country;
+    private String publishedAt;
     private List<TranslatedArticle> translatedArticles;
 
-    public Article(String headline, String originalContent, Source source, String author, String url) {
+    public Article(String headline,
+                   String originalContent,
+                   Source source,
+                   String author,
+                   String url,
+                   String country,
+                   String publishedAt) {
         this.headline = headline;
         this.originalContent = originalContent;
         this.source = source;
         this.author = author;
         this.URL = url;
+        this.country = country;
+        this.publishedAt = publishedAt;
         this.translatedArticles = new ArrayList<>();
     }
+
+    public String getCountry() { return country; }
+
+    public String getPublishedAt() { return publishedAt; }
 
     public String getHeadline() {
         return headline;
--- src/entity/TranslatedArticleFactory.java
+++ src/entity/TranslatedArticleFactory.java
@@ -1,7 +1,13 @@
 package entity;
 
 public class TranslatedArticleFactory implements TranslatedArticleFactoryInterface {
-    public TranslatedArticle create(String headline, String originalContent, Source source, String translatedLanguage, String author, String URL) {
+    public TranslatedArticle create(
+            String headline,
+            String originalContent,
+            Source source,
+            String translatedLanguage,
+            String author,
+            String URL) {
         return new TranslatedArticle(headline, originalContent, source, translatedLanguage, author, URL);
     }
 }
