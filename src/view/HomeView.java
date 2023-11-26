585da21d2dbd793144523c1b646b9ab67a426610
Jaiz183 <jaizepic@gmail.com>
2023-11-25 23:30:26 -0500

    Made major progress on view switching by incorporating a support and firing a property change event to switch views.

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -11,22 +11,25 @@ import use_case.translation.TranslationInputBoundary;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.text.View;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeView extends JPanel implements PropertyChangeListener{
     public final String viewName = "Home";
-    HomeViewModel homeViewModel;
+    private HomeViewModel homeViewModel;
     private ArticleRetrievalController articleRetrievalController;
     ArticleRetrievalPresenter articleRetrievalPresenter;
     private JList<String> headlinesUI;
-
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
     //Where the GUI is created:
 
     public HomeView(ArticleRetrievalController controller, HomeViewModel homeViewModel) {
@@ -43,6 +46,21 @@ public class HomeView extends JPanel implements PropertyChangeListener{
         JScrollPane listScroller = new JScrollPane(headlines);
         listScroller.setPreferredSize(new Dimension(250, 80));
         this.add(headlines);
+
+        headlines.addListSelectionListener(
+                new ListSelectionListener() {
+                    @Override
+                    public void valueChanged(ListSelectionEvent e) {
+                        if (headlines.getSelectedIndex() == -1) {
+                            System.out.println("Didn't select article.");
+                        } else {
+                            // Selection made.
+                            System.out.println("Selected article.");
+                            support.firePropertyChange("switchView", null, "ArticleView");
+                        }
+                    }
+                }
+        );
         headlinesUI = headlines;
 
         // Menu
@@ -131,5 +149,9 @@ public class HomeView extends JPanel implements PropertyChangeListener{
             } else {
             }
         }
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
     }
 }
