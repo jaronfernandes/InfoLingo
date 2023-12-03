6f301ee2ba0648c87c60db3adf97fe99d9f4be14
fern1394 <fernandesjaron@gmail.com>
2023-12-02 21:47:06 -0500

    Modified TranslateAPIDataAccessInterface to remove redundant method and to declare another throwable exception.

--- src/use_case/translation/TranslateAPIDataAccessInterface.java
+++ src/use_case/translation/TranslateAPIDataAccessInterface.java
@@ -3,7 +3,9 @@ package use_case.translation;
 import entity.Article;
 import entity.TranslatedArticle;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
 public interface TranslateAPIDataAccessInterface {
-    TranslatedArticle translateArticle(Article article, String language);
-    TranslatedArticle translateArticle(String headline, String language);
+    TranslatedArticle translateArticle(String headline, String language) throws NoSuchElementException, NullPointerException;
 }
