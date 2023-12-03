2e5e8aea432a82d7297f72d67bbada6e5fd4c097
Jaiz183 <jaizepic@gmail.com>
2023-12-03 13:17:52 -0500

    Worked on documentation by writing Javadocs, removing redundant print statements, adding comments where necessary and delegating to helper methods.

--- src/data_access/SummarisationDataAccessObject.java
+++ src/data_access/SummarisationDataAccessObject.java
@@ -1,9 +1,6 @@
 package data_access;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.Gson;
-import org.json.JSONObject;
 import com.google.gson.JsonParser;
 import use_case.summarization.SummarizationDataAccessInterface;
 
@@ -11,27 +8,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class SummarisationDataAccessObject implements SummarizationDataAccessInterface {
     private static final String API_TOKEN = "QwYkbpsOKMdgMxLnKJFjsKTcFiHCjAXtCMQPcXPwQrubCnjNOc";
-    private static final String baseUrl= "https://portal.ayfie.com/api/summarize";
+    private static final String BASE_URL = "https://portal.ayfie.com/api/summarize";
+    private final int variation = 20;
 
+    /**
+     * Method that returns a summary close to length in word count given content to summarise.
+     * @param content Content of an article to be summarised.
+     * @param length Word count to which summarisation occurs.
+     * @return A String representing summarised content.
+     * */
     @Override
     public String summarizeArticle(String content, Integer length) {
         String summarisedContent = "";
 
         try {
-            URL url = new URL(baseUrl);
+            URL url = new URL(BASE_URL);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
             // Specify request method.
@@ -41,14 +37,11 @@ public class SummarisationDataAccessObject implements SummarizationDataAccessInt
             httpURLConnection.setRequestProperty("Content-Type", "application/json");
             httpURLConnection.setRequestProperty("X-API-KEY", API_TOKEN);
 
-//            System.out.println(httpURLConnection.getResponseCode());
             httpURLConnection.setDoOutput(true);
 
-            String body = "{\"language\": \"auto\", \"text\": \"" + content + "\", \"min_length\": " + (length - 10) + ", \"max_length\": " + (length + 40) + "}";
-//            String body = "{\"language\": \"auto\", \"text\": \"" + content + "\", \"min_length\": 5, \"max_length\": 100}";
+            String body = getBody(content, length, variation);
 
-            System.out.println(body);
-
+            // Send request.
             try {
                 OutputStream os = httpURLConnection.getOutputStream();
                 byte[] input = body.getBytes(StandardCharsets.UTF_8);
@@ -63,22 +56,21 @@ public class SummarisationDataAccessObject implements SummarizationDataAccessInt
 //                System.out.println(entry.getKey() + ": " + entry.getValue());
 //            }
 
+            // Read output.
             try {
                 InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream(), StandardCharsets.UTF_8);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-                StringBuilder response = new StringBuilder();
+                StringBuilder data = new StringBuilder();
 
                 String currentLine;
                 while ((currentLine = bufferedReader.readLine()) != null) {
-                    response.append(currentLine.trim());
+                    data.append(currentLine.trim());
                 }
-//                System.out.println(response);
 
-                JsonParser jsonParser = new JsonParser();
-                JsonObject jsonObject = jsonParser.parse(response.toString()).getAsJsonObject();
-                summarisedContent = jsonObject.get("result").getAsString();
-                System.out.println(summarisedContent);
+                // Parse data and retrieve summary.
+                JsonObject response = parseResponse(data.toString());
+                summarisedContent = response.get("result").getAsString();
 
                 httpURLConnection.disconnect();
             } catch (Exception e) {
@@ -90,5 +82,33 @@ public class SummarisationDataAccessObject implements SummarizationDataAccessInt
         }
 
         return summarisedContent;
+    }
+
+    /**
+     * Method that returns a formatted body for HTTP request.
+     * @param content Content of an article to be summarised.
+     * @param length Word count to which summarisation occurs.
+     * @param variation Leniency of summary word count - lower the variation, more imprecise the summary word count.
+     * @return A String representing body to be sent with HTTP request.
+     * */
+    private String getBody(String content, int length, int variation) {
+        return "{\"language\": \"auto\", \"text\": \"" +
+                content +
+                "\", \"min_length\": " +
+                (length - variation) +
+                ", \"max_length\": " +
+                (length + variation) +
+                "}";
+    }
+
+    /**
+     * Method that parses data as JSON object.
+     * @param data Return value of HTTP request.
+     * @return A JSON object that represents the output of the HTTP request.
+     * */
+    private JsonObject parseResponse(String data) {
+        JsonParser jsonParser = new JsonParser();
+        JsonObject response = jsonParser.parse(data).getAsJsonObject();
+        return response;
     }
 }
--- src/use_case/summarization/SummarizationDataAccessInterface.java
+++ src/use_case/summarization/SummarizationDataAccessInterface.java
@@ -5,5 +5,11 @@ import entity.Article;
 import java.util.List;
 
 public interface SummarizationDataAccessInterface {
+    /**
+     * Method that returns a summary close to length in word count given content to summarise.
+     * @param content Content of an article to be summarised.
+     * @param length Word count to which summarisation occurs.
+     * @return A String representing summarised content.
+     * */
     String summarizeArticle(String content, Integer length);
 }
