8a20bd44a688db6b9ccbe5ec1d33c62f02435d35
Jaiz183 <jaizepic@gmail.com>
2023-12-02 23:52:31 -0500

    Completed HomeViewTest for everything planned except ranking. Ranking requires fiddling with preferences automatically. Ran HomeViewTest and the UI works perfectly.

--- test/view/HomeViewTest.java
+++ test/view/HomeViewTest.java
@@ -1,24 +1,42 @@
 package view;
 
 import app.ArticleRetrievalUseCaseFactory;
+import app.TranslationUseCaseFactory;
 import data_access.APIDataAccessObject;
+import data_access.SummarisationDataAccessObject;
 import interface_adapter.*;
+import use_case.summarization.SummarizationDataAccessInterface;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-public class HomeViewArticleRetrievalTest {
+public class HomeViewTest {
 
     @org.junit.Test
     public void testSignupView() {
 
         // Copy code in main.
+        // Application window.
+        JFrame application = new JFrame("InfoLingo");
+
+        // menu end
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // Instantiate views.
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
         APIDataAccessObject articleRetrievalDataAccessObject = new APIDataAccessObject();
+        SummarizationDataAccessInterface summarisationDataAccessObject = new SummarisationDataAccessObject();
 
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         HomeViewModel homeViewModel = new HomeViewModel(new HomeState(new ArrayList<>()));
@@ -27,13 +45,32 @@ public class HomeViewArticleRetrievalTest {
 
         HomeView homeView = ArticleRetrievalUseCaseFactory.create(viewManagerModel, homeViewModel, articleRetrievalDataAccessObject, groupingViewModel);
 
+        views.add(homeView, homeView.viewName);
+
+        ArticleViewModel articleViewModel = new ArticleViewModel(new ArticleState());
+
+        ViewManager viewManager = new ViewManager(views, cardLayout, viewManagerModel, homeView);
+
+        // Set initial view.
+        ArticleView articleView = TranslationUseCaseFactory.create(
+                viewManagerModel,
+                articleViewModel,
+                articleRetrievalDataAccessObject,
+                summarisationDataAccessObject
+        );
+
+        views.add(articleView, articleView.viewName);
+
         // Create UI.
-        JFrame jf = new JFrame();
-        jf.setContentPane(homeView);
-        jf.pack();
-        jf.setVisible(true);
+        viewManagerModel.setActiveView(homeView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setVisible(true);
 
         // Get a reference to search field and search button.
+//        Component[] components = homeView.getComponents();
+        JList headlines = (JList) homeView.getComponent(0);
         JMenuBar menuBar = (JMenuBar) homeView.getComponent(2);
         JTextField searchField = (JTextField) menuBar.getComponent(1);
         JButton searchButton = (JButton) menuBar.getComponent(2);
@@ -41,9 +78,9 @@ public class HomeViewArticleRetrievalTest {
         // Simulate click on search field.
         searchField.requestFocus();
 
-        // Pause execution for 2 seconds.
+        // Pause execution for 0.5 seconds.
         try {
-            sleep(2000);
+            sleep(500);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -62,9 +99,9 @@ public class HomeViewArticleRetrievalTest {
             // Tell menu bar to type something into the search field!
             menuBar.dispatchEvent(event);
 
-            // Pause execution for 2 seconds.
+            // Pause execution.
             try {
-                sleep(2000);
+                sleep(250);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -83,9 +120,9 @@ public class HomeViewArticleRetrievalTest {
             );
             searchField.dispatchEvent(eventRight);
 
-            // Pause execution for 2 seconds.
+            // Pause execution.
             try {
-                sleep(2000);
+                sleep(250);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -98,12 +135,31 @@ public class HomeViewArticleRetrievalTest {
 
         // Wait for request to return.
         try {
-            sleep(3000);
+            sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
         // Check that homeViewModel contains articles found.
         assertNotEquals(homeViewModel.getHomeState().getArticles(), new ArrayList<>());
+
+        // Try redirecting to an article.
+        headlines.setSelectedIndex(0);
+
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        headlines.setSelectedIndex(1);
+
+        // Wait for redirection.
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
