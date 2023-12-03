8abe42b4575b6f0c7598d5c91303c1efc7f1e6d7
fern1394 <fernandesjaron@gmail.com>
2023-12-02 21:45:24 -0500

    Removed article from parameter in constructor of the Translation input data.

--- src/use_case/translation/TranslationInputData.java
+++ src/use_case/translation/TranslationInputData.java
@@ -5,28 +5,15 @@ import interface_adapter.translation.TranslationController;
 
 public class TranslationInputData {
 
-    private Article article;
     private String language;
     private String headline;
-
-    public TranslationInputData(Article article, String language) {
-        this.language = language;
-        this.article = article;
-    }
 
     public TranslationInputData(String headline, String language) {
         this.language = language;
         this.headline = headline;
     }
-    public boolean articleExists() {
-        return article != null;
-    }
 
     public String getHeadline() { return headline; }
-
-    public Article getArticle() {
-        return article;
-    }
 
     public String getLanguage() {
         return language;
