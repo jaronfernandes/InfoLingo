33b153f02f51b58fa36e92bd115dc9d15b47d6cb
fern1394 <fernandesjaron@gmail.com>
2023-12-02 22:20:33 -0500

    Removed commented out code and unused import statements to clean up code and make it clean code.

--- src/data_access/APIDataAccessObject.java
+++ src/data_access/APIDataAccessObject.java
@@ -8,10 +8,7 @@ import entity.*;
 
 import use_case.article_retrieval.ArticleRetrievalDataAccessInterface;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -21,16 +18,13 @@ import org.json.JSONObject;
 import java.util.*;
 
 import org.json.*;
-import use_case.translation.TranslateAPIDataAccessInterface;
 
-import javax.print.URIException;
 import java.lang.Math;
 
 public class APIDataAccessObject implements ArticleRetrievalDataAccessInterface {
     private static final String BASE_URL = "https://newsapi.org/v2/";
     private final Integer numArticles = 10;
     private final static String API_TOKEN = "724da595748f4aaa9c5692d0aae9fffb";
-    private final static String DEEPL_TRANSLATE_API_KEY = "8dbcc2f3-03ef-8e22-3c4a-718f08bbe557:fx";
     private HashMap<String, Article> storedArticles = new HashMap<>();
     private static HashMap<String, HashMap<Article, TranslatedArticle>> storedTranslatedArticles = new HashMap<>();
 
@@ -165,101 +159,4 @@ public class APIDataAccessObject implements ArticleRetrievalDataAccessInterface 
     protected HashMap<String, HashMap<Article, TranslatedArticle>> getStoredTranslatesArticles() {
         return storedTranslatedArticles;
     }
-
-    /**
-     * <p> Returns a TranslatedArticle object from the given headline. Assumes the article has already been searched
-     * for and exists in the data access object. Throws a NoSuchElementException if cannot retrieve article by headline.
-     * </p>
-     * @param headline An article headline as a String.
-     * @param language A String indicating the language in the ISO 639 format.
-     * @throws NoSuchElementException if no article with the headline is found.
-     * @throws NullPointerException if the API failed to translate the article.
-     * @author Jaron Fernandes
-     */
-//    @Override
-//    public TranslatedArticle translateArticle(String headline, String language) throws NoSuchElementException, NullPointerException {
-//        Article article = retrieveArticleByHeadline(headline);
-//
-//        System.out.println(language);
-//        String translatedHeadline = null, translatedContent = null;
-//        TranslatedArticleFactory translatedArticleFactory = new TranslatedArticleFactory();
-//
-//        // TEST
-//        String modifiedHeadline = article.getHeadline().replaceAll("[^A-Za-z0-9_@./#&+\\-\\[\\]\\s]", "");
-//        String modifiedContent = article.getContent().replaceAll("[^A-Za-z0-9_@./#&+\\-\\[\\]\\s]", "");
-////        translatedContent = article.getContent();
-////        System.out.println(translatedContent);
-//
-//        if (storedTranslatedArticles.containsKey(language)) {
-//            if (storedTranslatedArticles.get(language).containsKey(article)) {
-//                return storedTranslatedArticles.get(language).get(article);
-//            }
-//        }
-//
-//        try {
-//            translatedHeadline = translateText(modifiedHeadline, language);
-//            translatedContent = translateText(modifiedContent, language);
-//        }
-//        catch (Exception e) {
-//            // try using Google's instead as a backup! (due to char limit on DeepL)
-//            // TODO: API call for translating content (need Gradle/Maven dependency thing)
-//            try {
-//
-//            } catch (Exception e2) {
-//
-//            }
-//        }
-//        if (translatedContent == null || translatedHeadline == null) {
-//            // Throw a null pointer exception if unable to retrieve any translated text.
-//            throw new NullPointerException();
-//        }
-//        TranslatedArticle finishedTranslatedArticle = translatedArticleFactory.create(
-//                translatedHeadline,
-//                translatedContent,
-//                article.getSource(),
-//                language,
-//                article.getAuthor(),
-//                article.getURL(),
-//                article.getCountry(),
-//                article.getPublishedAt()
-//        );
-//
-//        if (storedTranslatedArticles.containsKey(language)) {
-//            storedTranslatedArticles.get(language).put(article, finishedTranslatedArticle);
-//        }
-//        else {
-//            HashMap<Article, TranslatedArticle> newTranslatedArticles = new HashMap<>();
-//            newTranslatedArticles.put(article, finishedTranslatedArticle);
-//            storedTranslatedArticles.put(language, newTranslatedArticles);
-//        }
-////        storedArticles.put(finishedTranslatedArticle.getHeadline(), finishedTranslatedArticle);
-//
-//        return finishedTranslatedArticle;
-//    }
-//
-//    private String translateText(String text, String language) throws URISyntaxException, IOException, InterruptedException {
-//        String formattedText = String.join("%20", text.split(" "));
-//        // System.out.println(formattedText);
-//
-//        URI uri = new URI("https://api-free.deepl.com/v2/translate?auth_key=" + DEEPL_TRANSLATE_API_KEY +
-//                "&text=" + formattedText + "&target_lang=" + language);
-//        HttpClient client = HttpClient.newHttpClient();
-//        HttpRequest request = HttpRequest.newBuilder().
-//                uri(uri).
-//                GET().
-//                build();
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        String body = response.body();
-//        JSONObject data = new JSONObject(body);
-//
-////        System.out.println(data);
-////        System.out.println(language);
-//
-//        JSONArray articles = data.getJSONArray("translations");
-//        JSONObject translation = articles.getJSONObject(0);
-////        String source_language = translation.getString("detected_source_language");
-//        // Note: line above is not needed since it's the same as the language we wanted to translate to!
-//
-//        return translation.getString("text");
-//    }
 }
