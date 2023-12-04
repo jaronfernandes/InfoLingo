1c4971ac744d4dab01799c30e3d25542762f02c2
fern1394 <fernandesjaron@gmail.com>
2023-12-03 21:50:59 -0500

    Moved javadocs from article/translated entity factories to their interfaces, so they're implemented in the concrete classes anyway.

--- src/entity/ArticleFactory.java
+++ src/entity/ArticleFactory.java
@@ -1,16 +1,6 @@
 package entity;
 
 public class ArticleFactory implements ArticleFactoryInterface {
-    /**
-     * An unmodified Article entity factory
-     * @param headline String of the headline
-     * @param originalContent String of the originalContent
-     * @param source Source object of the source
-     * @param author String of the author
-     * @param url String of the url
-     * @param country String of the country
-     * @param publishedAt String of the publishedAt
-     */
     public Article create(String headline,
                           String originalContent,
                           Source source,
--- src/entity/TranslatedArticleFactory.java
+++ src/entity/TranslatedArticleFactory.java
@@ -1,17 +1,6 @@
 package entity;
 
 public class TranslatedArticleFactory implements TranslatedArticleFactoryInterface {
-    /**
-     * A translated Article entity factory
-     * @param headline String of the headline
-     * @param originalContent String of the content
-     * @param source Source object of the source
-     * @param translatedLanguage String of the language translated to.
-     * @param author String of the author
-     * @param URL String of the URL
-     * @param country String of the country
-     * @param publishedAt String of the publishedAt
-     */
     @Override
     public TranslatedArticle create(
             String headline,
