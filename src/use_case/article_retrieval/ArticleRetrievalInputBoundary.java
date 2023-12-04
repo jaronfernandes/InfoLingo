15f530f86aa3269932604ffbea7b19040a171b6c
fern1394 <fernandesjaron@gmail.com>
2023-12-03 21:03:06 -0500

    Added javadocs here so that TranslationInteractor could implement them.

--- src/use_case/article_retrieval/ArticleRetrievalInputBoundary.java
+++ src/use_case/article_retrieval/ArticleRetrievalInputBoundary.java
@@ -1,5 +1,12 @@
 package use_case.article_retrieval;
 
 public interface ArticleRetrievalInputBoundary {
+    /**
+     * Executes the use-case. If successful, it will call the ArticleRetrievalPresenter to update the HomeView
+     * based on the output data. If it failed, it calls the view to display the fail view to the user.
+     *
+     * @param inputData The ArticleRetrievalInputData needed through dependency injection from the controller.
+     * @author Jaron Fernandes, Seth Blatt
+     */
     void execute(ArticleRetrievalInputData inputData);
 }
