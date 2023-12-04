62e09094a49b71b30ed01aeb8dca0eac4138bacd
Jaiz183 <jaizepic@gmail.com>
2023-12-03 20:18:10 -0500

    Completed java docs and cleaned up code, including removing unnecessary print statements.

--- src/interface_adapter/summarization/SummarizationController.java
+++ src/interface_adapter/summarization/SummarizationController.java
@@ -6,17 +6,27 @@ import use_case.summarization.SummarizationInputData;
 public class SummarizationController {
     final SummarizationInputBoundary summarizationInteractor;
 
-
     public SummarizationController(SummarizationInputBoundary articleRetrievalInteractor) {
         this.summarizationInteractor = articleRetrievalInteractor;
     }
 
+
+    /**
+     * Method that packages and cleans data from ArticleView for data processing after passing to interactor.
+     * @param content Content of article to be summarised extracted from a text area in ArticleView.
+     * @param length Number of words to which summary is restricted extracted from slider in ArticleView.
+     */
     public void execute(String content, Integer length) {
         String cleanContent = cleanContent(content);
         SummarizationInputData articleRetrievalInputData = new SummarizationInputData(cleanContent, length);
         summarizationInteractor.execute(articleRetrievalInputData);
     }
 
+    /**
+     * Method that removes problematic whitespace characters that are not allowed in an HTTP request body such as \n.
+     * @param dirtyContent Unprocessed article content.
+     * @return Processed article content.
+     */
     private String cleanContent(String dirtyContent) {
         // Replace all whitespace character except space with space.
         String cleanContent = dirtyContent.replaceAll("[\\t\\n\\r\\f\\v]", " ");
