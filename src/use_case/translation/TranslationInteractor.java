370dc88decd601a2a1de1d42b92702bef50aa1e3
fern1394 <fernandesjaron@gmail.com>
2023-12-03 20:23:12 -0500

    Wrote javadocs for translation interactor.

--- src/use_case/translation/TranslationInteractor.java
+++ src/use_case/translation/TranslationInteractor.java
@@ -9,12 +9,27 @@ public class TranslationInteractor implements TranslationInputBoundary {
     private TranslationOutputBoundary presenter;
     private TranslateAPIDataAccessInterface translationDataAccessObject;
 
+    /**
+     * The translation interactor is used to handle the translation use case, by calling the API methods to access articles
+     * and to translate them through the API.
+     *
+     * @param outputBoundary The translation presenter object needed through dependency injection.
+     * @param translationDataAccessObject The translation DAO.
+     * @author Jaron Fernandes
+     */
     public TranslationInteractor(TranslationOutputBoundary outputBoundary,
                                  TranslateAPIDataAccessInterface translationDataAccessObject) {
         this.translationDataAccessObject = translationDataAccessObject;
         this.presenter = outputBoundary;
     }
 
+    /**
+     * Executes the use-case. If successful, it will call the TranslationPresenter to update the ArticleView
+     * based on the output data. If it failed, it calls the view to display the fail view to the user.
+     *
+     * @param inputData The TranslationInputData needed through dependency injection from the controller.
+     * @author Jaron Fernandes
+     */
     @Override
     public void execute(TranslationInputData inputData) {
         try {
