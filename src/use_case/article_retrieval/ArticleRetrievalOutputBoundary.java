3e42c307ba497acc3ac6f4292f7ccb8f4f0892e0
Jaiz183 <jaizepic@gmail.com>
2023-12-03 20:18:29 -0500

    Completed java docs and cleaned up code, including removing unnecessary print statements.

--- src/use_case/article_retrieval/ArticleRetrievalOutputBoundary.java
+++ src/use_case/article_retrieval/ArticleRetrievalOutputBoundary.java
@@ -1,7 +1,15 @@
 package use_case.article_retrieval;
 
 public interface ArticleRetrievalOutputBoundary {
-
+    /**
+     * Changes parameters in view model's state and calls fires a signal to view indicating that parameters have been changed.
+     * @param outputData Contains data to be displayed in new ArticleView.
+     */
     void prepareSuccessView(ArticleRetrievalOutputData outputData);
+
+    /**
+     * In case of error in use case, tells view to display an appropriate message containing the string provided by firing a signal.
+     * @param error Message describing error due to which use case failed.
+     */
     void prepareFailView(String error);
 }
