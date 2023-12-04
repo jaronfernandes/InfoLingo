7cf98fea740e12901ce57db726a8a0cbc25692bc
Dominic Le <dominic.le@mail.utoronto.ca>
2023-12-03 19:41:28 -0500

    added tests to have > 94% lines coverage.

--- test/use_case/RankingInteractorTest.java
+++ test/use_case/RankingInteractorTest.java
@@ -28,18 +28,11 @@ public class RankingInteractorTest {
                 fail("Use case should have passed. Unexpected failure.");
             }
         };
-
         List<String> countries = new ArrayList<>();
         countries.add("ca");
         String date = "1003-02-11";
-        List<Article> articles = new ArrayList<>();
-        Article distract1 = new Article("Roblox bad", "Roblox is not great!", new Source("EvilCorp", "en"), "Jill", "https://roblox.com", "cn", "2031-11-02");
-        Article distract2 = new Article("Roblox good", "Roblox is  great!", new Source("AwesomePeople", "en"), "Jessie", "https://roblox.com", "ca", "1003-02-11");
-        Article target = new Article("Roblox alright", "Roblox is  alright!", new Source("Lamos", "en"), "Jessie", "https://roblox.com", "fr", "1003-02-11");
-        articles.add(distract1);
-        articles.add(target);
-        articles.add(distract2);
-        RankingInputData rankingInputData = new RankingInputData(countries, date, articles);
+
+        RankingInputData rankingInputData =  getRankingInputDataWRoblox(countries, date);
         RankingInteractor rankingInteractor = new RankingInteractor(rankingPresenter);
         rankingInteractor.execute(rankingInputData);
 
@@ -59,19 +52,86 @@ public class RankingInteractorTest {
 
 
         };
-        List<String> weirdCountries = new ArrayList<>();
-        weirdCountries.add("us");
-        String date = "1003-02-11";
+        List<String> countries = new ArrayList<>();
+        countries.add("us");
+        String date = "YYYY-MM-DD";
+
+        RankingInputData rankingInputData =  getRankingInputDataWRoblox(countries, date);
+        RankingInteractor rankingInteractor = new RankingInteractor(rankingPresenter);
+        rankingInteractor.execute(rankingInputData);
+
+    }
+    @Test
+    public void emptyCountriesTest(){
+        RankingOutputBoundary rankingPresenter = new RankingOutputBoundary() {
+            @Override
+            public void prepareSuccessView(RankingOutputData outputData) {
+                assertEquals("Roblox bad", outputData.getRanking().get(0).getHeadline());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case should have passed. Unexpected failure.");
+            }
+        };
+        List<String> countries = new ArrayList<>();
+        String date = "2031-11-02";
+
+        RankingInputData rankingInputData =  getRankingInputDataWRoblox(countries, date);
+        RankingInteractor rankingInteractor = new RankingInteractor(rankingPresenter);
+        rankingInteractor.execute(rankingInputData);
+    }
+
+    @Test
+    public void emptyCountriesYearOnlyTest(){
+        RankingOutputBoundary rankingPresenter = new RankingOutputBoundary() {
+            @Override
+            public void prepareSuccessView(RankingOutputData outputData) {
+                assertEquals("Roblox good", outputData.getRanking().get(0).getHeadline());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case should have passed. Unexpected failure.");
+            }
+        };
+        List<String> countries = new ArrayList<>();
+        String date = "1003";
+
+        RankingInputData rankingInputData =  getRankingInputDataWRoblox(countries, date);
+        RankingInteractor rankingInteractor = new RankingInteractor(rankingPresenter);
+        rankingInteractor.execute(rankingInputData);
+    }
+    @Test
+    public void emptyCountriesMonthOnlyTest(){
+        RankingOutputBoundary rankingPresenter = new RankingOutputBoundary() {
+            @Override
+            public void prepareSuccessView(RankingOutputData outputData) {
+                assertEquals("Roblox good", outputData.getRanking().get(0).getHeadline());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case should have passed. Unexpected failure.");
+            }
+        };
+        List<String> countries = new ArrayList<>();
+        String date = "1003-02";
+
+        RankingInputData rankingInputData =  getRankingInputDataWRoblox(countries, date);
+        RankingInteractor rankingInteractor = new RankingInteractor(rankingPresenter);
+        rankingInteractor.execute(rankingInputData);
+    }
+
+    private static RankingInputData getRankingInputDataWRoblox(List<String> countries, String date) {
         List<Article> articles = new ArrayList<>();
         Article distract1 = new Article("Roblox bad", "Roblox is not great!", new Source("EvilCorp", "en"), "Jill", "https://roblox.com", "cn", "2031-11-02");
-        Article distract2 = new Article("Roblox good", "Roblox is  great!", new Source("AwesomePeople", "en"), "Jessie", "https://roblox.com", "fr", "1003-02-11");
-        Article distract3 = new Article("Roblox alright", "Roblox is  great!", new Source("AwesomePeople", "en"), "Jessie", "https://roblox.com", "ca", "1003-02-11");
+        Article distract2 = new Article("Roblox good", "Roblox is  great!", new Source("AwesomePeople", "en"), "Jessie", "https://roblox.com", "ca", "1003-02-11");
+        Article distract3 = new Article("Roblox alright", "Roblox is  great!", new Source("AwesomePeople", "en"), "Jessie", "https://roblox.com", "fr", "1003-02-11");
         articles.add(distract1);
         articles.add(distract2);
         articles.add(distract3);
-        RankingInputData rankingInputData1 =  new RankingInputData(weirdCountries, date, articles);
-        RankingInteractor rankingInteractor1 = new RankingInteractor(rankingPresenter);
-        rankingInteractor1.execute(rankingInputData1);
+        return new RankingInputData(countries, date, articles);
+    }
 
-    }
-    }
+}
