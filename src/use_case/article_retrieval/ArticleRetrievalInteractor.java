974a03bf17823df1c49aa55a21d3b5b5a57d3f64
fern1394 <fernandesjaron@gmail.com>
2023-12-03 21:03:53 -0500

    Moved javadocs to the input boundary.

--- src/use_case/article_retrieval/ArticleRetrievalInteractor.java
+++ src/use_case/article_retrieval/ArticleRetrievalInteractor.java
@@ -23,13 +23,6 @@ public class ArticleRetrievalInteractor implements ArticleRetrievalInputBoundary
         this.presenter = outputBoundary;
     }
 
-    /**
-     * Executes the use-case. If successful, it will call the ArticleRetrievalPresenter to update the HomeView
-     * based on the output data. If it failed, it calls the view to display the fail view to the user.
-     *
-     * @param inputData The ArticleRetrievalInputData needed through dependency injection from the controller.
-     * @author Jaron Fernandes, Seth Blatt
-     */
     @Override
     public void execute(ArticleRetrievalInputData inputData) {
         List<Article> articles = articleRetrievalDataAccessObject.getArticles(inputData.getQuery());
