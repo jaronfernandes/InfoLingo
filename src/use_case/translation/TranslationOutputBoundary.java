f37c497e70939098c8a1ad51b3dbf2f3e19a127b
fern1394 <fernandesjaron@gmail.com>
2023-12-03 21:02:44 -0500

    Added javadocs here so that TranslationPresenter could implement them.

--- src/use_case/translation/TranslationOutputBoundary.java
+++ src/use_case/translation/TranslationOutputBoundary.java
@@ -2,6 +2,18 @@ package use_case.translation;
 
 public interface TranslationOutputBoundary {
 
+    /**
+     * Fires a signal to the article view model to update the view accordingly.
+     *
+     * @param outputData The TranslationOutputData to unpack and update the state accordingly with the data given.
+     * @author Jaron Fernandes
+     */
     void prepareSuccessView(TranslationOutputData outputData);
+    /**
+     * Fires a signal to the article view model to update the view accordingly.
+     *
+     * @param error The error message as a string.
+     * @author Jaron Fernandes
+     */
     void prepareFailView(String error);
 }
