08cf015c33db4ba48daf01521d44e6aa6157566c
Jaiz183 <jaizepic@gmail.com>
2023-12-03 20:19:38 -0500

    Completed java docs and cleaned up code, including removing unnecessary print statements.

--- src/use_case/summarization/SummarizationInputBoundary.java
+++ src/use_case/summarization/SummarizationInputBoundary.java
@@ -1,5 +1,9 @@
 package use_case.summarization;
 
 public interface SummarizationInputBoundary {
+    /**
+     * Checks for problematic summary word counts before sending content for summarisation to SummarisationDataAccessInterface.
+     * @param inputData Contains important data for processing, including summary word count and article content.
+     */
     void execute(SummarizationInputData inputData);
 }
